3aebc9c13d57b7d57f8453e0e0708e8faa9c27ab
antonnn1 <antonnn@vgy.se>
2017-01-27 14:49:01 +0100

    summanUppTillTusen

--- tio_bra_ovningar/Program.cs
+++ tio_bra_ovningar/Program.cs
@@ -10,6 +10,16 @@ namespace tio_bra_ovningar
     {
         static void Main(string[] args)
         {
+            /* Yassin Oummadi & Anton Nordin 27 Januari 2017*/
+            int summa = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if(i % 5 == 0 || i % 3 == 0)
+                {
+                    summa = summa + i;
+                }
+            }
+            Console.WriteLine("Summan av alla tal under 1000 som är delbara med 3 och/eller 5 är: " + summa);
         }
     }
 }
